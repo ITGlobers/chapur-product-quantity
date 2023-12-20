987fd0dc917db5e516ed2ac0de0632da953f98cc
Edwin Miranda <edwincolombo@hotmail.com>
2023-12-20 13:24:08 -0500

    release: chapurmx v0.0.0
    Limit digital products to 36 in MDR and for B2C with physical inventory

--- react/components/BaseProductQuantity.tsx
+++ react/components/BaseProductQuantity.tsx
@@ -1,8 +1,9 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { FormattedMessage } from 'react-intl'
 import { useCssHandles } from 'vtex.css-handles'
 import { DispatchFunction } from 'vtex.product-context/ProductDispatchContext'
 import { ProductContext } from 'vtex.product-context'
+import { OrderForm } from 'vtex.order-manager'
 
 import DropdownProductQuantity from './DropdownProductQuantity'
 import StepperProductQuantity from './StepperProductQuantity'
@@ -11,9 +12,19 @@ export type NumericSize = 'small' | 'regular' | 'large'
 export type SelectorType = 'stepper' | 'dropdown'
 export type QuantitySelectorStepType = 'unitMultiplier' | 'singleUnit'
 
+interface referenceId {
+  Key: string
+  Value: string 
+}
+interface CustomSelectedItem{
+  referenceId: referenceId[]
+}
+
+type CustomSelectedItemExtended = CustomSelectedItem & ProductContext['selectedItem'];
+
 export interface BaseProps {
   dispatch: DispatchFunction
-  selectedItem?: ProductContext['selectedItem']
+  selectedItem?: CustomSelectedItemExtended
   showLabel?: boolean
   selectedQuantity: number
   selectorType?: SelectorType
@@ -32,6 +43,12 @@ const CSS_HANDLES = [
 export type OnChangeCallback = {
   value: number
 }
+
+
+const { useOrderForm } = OrderForm
+
+const VIRTUAL_MAX_VALUE = 36
+const DEFAULT_GIFT_TABLE_ID = 'default-gift-table-id'
 
 const BaseProductQuantity: StorefrontFunctionComponent<BaseProps> = ({
   dispatch,
@@ -52,18 +69,40 @@ const BaseProductQuantity: StorefrontFunctionComponent<BaseProps> = ({
     [dispatch]
   )
 
-  const availableQuantity =
-    selectedItem?.sellers?.find(({ sellerDefault }) => sellerDefault === true)
-      ?.commertialOffer?.AvailableQuantity ?? 0
+  const [availableQuantity, setAvailableQuantity] = useState(selectedItem?.sellers?.find(({ sellerDefault }) => sellerDefault === true)
+  ?.commertialOffer?.AvailableQuantity ?? 0)
+   
+  const orderForm = useOrderForm()
+  
+  const mdr = orderForm?.orderForm?.customData?.customApps?.find((app: {id: string}) =>  app?.id === 'mdr')
 
-  if (availableQuantity < 1 || !selectedItem) {
+  if (!selectedItem) {
     return null
   }
 
+  useEffect(() => {
+    const giftTableId = mdr?.fields?.giftTableId ?? DEFAULT_GIFT_TABLE_ID
+    const refId = selectedItem?.referenceId[0].Value
+    const skuId = selectedItem.itemId
+
+    if(!refId || !skuId) return
+
+    const getIsVirtual = async () => {
+      const res = await fetch(`/chapur/v1/sku-virtual/${giftTableId}/${refId}/${skuId}`)
+      const data = await res.json()
+      const virtual = data.virtual ?? false
+      const physicalInventory = data.physicalInventory ?? 0
+
+      setAvailableQuantity(virtual ? VIRTUAL_MAX_VALUE : physicalInventory)
+    }
+
+    getIsVirtual()
+  }, [mdr, selectedItem.itemId])
+  
   const showAvailable = availableQuantity <= warningQuantityThreshold
   const unitMultiplier =
     quantitySelectorStep === 'singleUnit' ? 1 : selectedItem.unitMultiplier
-
+  
   return (
     <div
       className={`${handles.quantitySelectorContainer} flex flex-column mb4`}>
@@ -84,7 +123,7 @@ const BaseProductQuantity: StorefrontFunctionComponent<BaseProps> = ({
           onChange={onChange}
         />
       )}
-      {selectorType === 'dropdown' && (
+      {selectorType === 'dropdown' && availableQuantity > 0 && (
         <DropdownProductQuantity
           itemId={selectedItem.itemId}
           selectedQuantity={selectedQuantity}
